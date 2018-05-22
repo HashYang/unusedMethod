88b265f26b3799cc1ce1e3b2a2d52bc569ee6c6b
HashYang <yangzhk1024@gmail.com>
2018-05-22 14:46:48 +0800

    Add files via upload
    objc unusedMethod 
    python objc_cover.py [linkmap.txt] [mach-o Path]

---
+++ objc_cover.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python
+
+__author__ = "Hash"
+__date__ = "2018-05-22"
+__license__ = "GPL"
+
+import os
+import re
+import shutil
+import sys
+import tempfile
+
+def verified_macho_path(args):
+    if len(sys.argv) != 2:
+        return None
+
+    path = sys.argv[1]
+
+    if not os.path.isfile(path):
+        return None
+
+    ## Apparently there is a bug in otool -- it doesn't seem to like executables
+    ## with spaces in the names. If this is the case, make a copy and analyze that.
+    if ' ' in os.path.basename(path):
+        ## don't remove the spaces, that could lead to an empty string
+        new_filename = path.replace(' ', '_')
+        new_path = os.path.join(tempfile.mkdtemp(), new_filename)
+        shutil.copy(path, new_path)
+        path = new_path
+
+    cmd = "/usr/bin/file -b %r" % path
+    s = os.popen(cmd).read()
+
+    if not s.startswith('Mach-O'):
+        return None
+    
+    return path
+
+def signature_cmp(m1, m2):
+    cls1 = m1[2:].split(' ')[0]
+    cls2 = m2[2:].split(' ')[0]
+    
+    result = cmp(cls1, cls2)
+
+    if result == 0: # same class
+        if m1.startswith('+') and m2.startswith('-'):
+            return -1
+        elif m1.startswith('-') and m2.startswith('+'):
+            return +1
+        else: # same sign
+            return cmp(m1, m2)
+    
+    return result
+
+def implemented_methods(path1):
+    """
+    returns {'sel1':[sig1, sig2], 'sel2':[sig3]}
+    """
+    
+    re_sig_sel_ios = re.compile("\[.+\]\s([+|-]\[.+\s(.+)\])")
+    impl = {} # sel -> clsmtd
+
+    fd=open(path1)
+
+    for line in fd.readlines():
+        results = re_sig_sel_ios.findall(line)
+
+        if not results:
+            continue
+        (sig, sel) = results[0]
+        
+        if sel in impl:
+            impl[sel].append(sig)
+        else:
+            impl[sel] = [sig]
+    
+    return impl
+
+def referenced_selectors(path):
+    
+    re_sel = re.compile("__TEXT:__objc_methname:(.+)")
+    
+    refs = set()
+    
+    lines = os.popen("/usr/bin/otool -v -s __DATA __objc_selrefs %s" % path).readlines() # ios & mac
+
+    for line in lines:
+        results = re_sel.findall(line)
+        if results:
+            refs.add(results[0])
+    
+    return refs
+
+def potentially_unreferenced_methods():
+    implemented = implemented_methods(path1)
+    
+    if not implemented:
+        print "# can't find implemented methods"
+        sys.exit(1)
+    
+    referenced = referenced_selectors(path)
+    
+    l = []
+    
+    #print "-- implemented:", len(implemented)
+    #print "-- referenced:", len(referenced)
+
+    for sel in implemented:
+        if sel not in referenced:
+            for method in implemented[sel]:
+                l.append(method)
+                
+    l.sort(signature_cmp)
+    
+    return l
+
+if __name__ == "__main__":
+    
+    path = sys.argv[2]
+    path1 = sys.argv[1]
+
+    if not path:
+        print "Usage: %s MACH_O_FILE" % sys.argv[0]
+        sys.exit(1)
+    if not path1:
+            print "Usage: %s MACH_O_FILE" % sys.argv[0]
+            sys.exit(1)
+    
+    methods = potentially_unreferenced_methods()
+        
+    print "# the following methods may be unreferenced"
+    for m in methods:
+        print m
